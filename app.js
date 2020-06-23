2536524a7a04996ea431a5d32b89cd65626f47dd
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-06-23 14:03:36 +0300

    store and retrive objects data using local storage

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 'use strict';
 
-
 //get Images elements
 var imageBoxs = document.getElementsByClassName('box-up');
 var votBtns = document.getElementsByClassName('cart');
-
 
 // product Object decleration
 var products = [];
 var previousProducts = [];
 var totalClicks = 0;
 var productsName = [];
-var clicksArr =[];
-var shownArr=[];
+var clicksArr = [];
+var shownArr = [];
+var retrievedProducts;
 
 function product(name, path) {
   this.name = name;
@@ -22,6 +21,15 @@ function product(name, path) {
   products.push(this);
   productsName.push(this.name);
 }
+
+product.prototype.setItBack = function () {
+  for (var i = 0; i < retrievedProducts.length; i++) {
+    if (retrievedProducts[i].name === this.name) {
+      this.clicks = retrievedProducts[i].clicks;
+      this.shown = retrievedProducts[i].shown;
+    }
+  }
+};
 
 // ----------------- Creating the objects ------------------
 new product('bag', 'img/bag.jpg');
@@ -44,31 +52,27 @@ new product('usb', 'img/usb.gif');
 new product('water-can', 'img/water-can.jpg');
 new product('wine-glass', 'img/wine-glass.jpg');
 
+retrieveProduct();
 //dispalying the 3 products for the first time
 displayProducts();
 
 // ********************* functions section *********************
 
-
 // ------------------------- Function to change products -------------------------
 function changeProducts() {
   totalClicks += 1;
+  console.log(totalClicks);
   // If he has finished all 25 clicks, statistics will be shown
   if (totalClicks === 26) {
     for (var y = 0; y < 3; y++) {
       votBtns[y].onclick = null;
     }
-
-    // store clicks and showns
-    for (var i = 0; i < products.length; i++) {
-      shownArr.push(products[i].shown);
-      clicksArr.push(products[i].clicks);
-    }
+    storeProducts();
     displayStatisticsChart();
     return;
   }
   var itemClicked = event.target.id;
-  console.log(itemClicked);
+
   for (var z = 0; z < products.length; z++) {
     if (Number(itemClicked) === z) {
       products[z].clicks += 1;
@@ -128,10 +132,15 @@ function checkExistence(e) {
 
 // -------------------- Charts Section -----------------------
 
-function displayStatisticsChart(){
+function displayStatisticsChart() {
+  // store clicks and showns
+  for (var i = 0; i < products.length; i++) {
+    shownArr.push(products[i].shown);
+    clicksArr.push(products[i].clicks);
+  }
   var ctx = document.getElementById('myChart').getContext('2d');
   var chart = new Chart(ctx, {
-  // The type of chart we want to create
+    // The type of chart we want to create
     type: 'bar',
 
     // The data for our dataset
@@ -149,13 +158,32 @@ function displayStatisticsChart(){
           backgroundColor: '#4b134f',
           borderColor: '#4b134f',
           data: shownArr,
-        }
+        },
       ],
     },
 
     // Configuration options go here
     options: {},
   });
-
 }
 
+// -------------------- Function to Store in Local Storage -----------------------
+
+function storeProducts() {
+  var productsString = JSON.stringify(products);
+  localStorage.setItem('products', productsString);
+}
+
+//  Function that checks if there is products that saved in the local storage
+// and if is it true it will call an object function that will set back the values to the objects
+function retrieveProduct() {
+  retrievedProducts = JSON.parse(localStorage.getItem('products'));
+  if (!retrievedProducts) {
+    console.log('there is no item with this name');
+    return;
+  }
+  for (var i = 0; i < products.length; i++) {
+    products[i].setItBack();
+  }
+  displayStatisticsChart();
+}
